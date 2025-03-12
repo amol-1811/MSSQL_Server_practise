b26b51187319558ff1e795b3bfd8fb808da290bc
Amol Gosavi <87413926+amol-1811@users.noreply.github.com>
2025-03-12 14:37:48 +0530

    add two more queries

--- DISTINCT_Clause.sql
+++ DISTINCT_Clause.sql
@@ -15,3 +15,10 @@ SELECT DISTINCT city, state FROM sales.customers;
 --using SELECT DISTINCT with NULL
 SELECT DISTINCT phone FROM sales.customers ORDER BY phone;
 
+--DISTINCT vs GROUP BY
+SELECT city, state, zip_code FROM sales.customers
+GROUP BY city, state, zip_code
+ORDER BY city, state, zip_code;
+
+SELECT DISTINCT city, state, zip_code 
+FROM sales.customers;
